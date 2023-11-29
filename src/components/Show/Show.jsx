c1fb86b1f8caf0b3ce99bc582e5a41c10cdb4e85
Nugget-Meister <tremainecamacho@gmail.com>
2023-11-29 01:34:20 -0500

    prototyping of apicalls.js

--- src/components/Show/Show.jsx
+++ src/components/Show/Show.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { useParams } from 'react-router';
+import { getSingleClothing } from '../../helpers/apicalls';
 
 const Show = () => {
+    const {id} = useParams()
+    // console.log(id)
+
     return (
-        <div>
-            
+        <div className='Show'>
+            <Row 
+                className=''>
+                
+                <Container as={Col}>
+                    sss
+                </Container>
+                <Container as={Col}>
+                    sss
+                </Container>
+            </Row>
         </div>
     );
 }
